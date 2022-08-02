0c87b7ed4ad8b5776788c64d87bbbc9b076a2a15
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-08-02 13:26:30 -0500

    login and logout js done

--- server.js
+++ server.js
@@ -2,29 +2,37 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
+const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
-
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 3002;
-
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
   secret: "It is a secret secret",
-  cookie: {},
-  resave: false,
+  cookie: {
+    expires: 15 * 60 * 1000,
+  },
+  resave: true,
+  rolling: true,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
 };
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session(sess));
-app.use(require("./controllers"));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(routes);
+
+//app.use(require("./controllers"));
+
 //
 
 sequelize.sync({ force: true }).then(() => {
