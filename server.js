12f7591c1d47d0693bbe53a2a8ae2bf588c9a350
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-08-02 12:41:18 -0500

    need a tutor to take a look at my error

--- server.js
+++ server.js
@@ -8,13 +8,25 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+const sess = {
+  secret: "It is a secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-//turn on routes
+app.use(session(sess));
+app.use(require("./controllers"));
+app.use(routes);
 //
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Listening on port"));
 });
